07943e998987cf732032a08153c155a99a026b34
Calandra Stavroudis <calandra.st@gmail.com>
2021-02-25 11:11:27 -0500

    added webpack

---
+++ client/components/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class Home extends React.Component {
+  constructor() {
+    this.state = {
+      isLoggedIn: false,
+      user: null,
+    };
+    this.googleLogin = this.googleLogin.bind(this);
+  }
+
+  async googleLogin() {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const { user } = await firebase.auth().signInWithPopup(provider);
+      this.setState({ isLoggedIn: true, user: user });
+      document.write(`Hello ${user.displayName}`);
+      console.log("user", user);
+    } catch (error) {
+      console.log("error has occurred in googleLogin func:", error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoggedIn ? (
+          <div>
+            <h1>Welcome to the Firebase AWP, {this.state.user.displayName}!</h1>
+          </div>
+        ) : (
+          <div>
+            <h1>Welcome to the Firebase AWP</h1>
+            <button onclick={this.googleLogin()}>Login with Google</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Home;
--- public/app.js
+++ public/app.js
@@ -1,43 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import Home from "../client/components/Home";
 
-class Webpage extends React.Component {
-  constructor() {
-    this.state = {
-      isLoggedIn: false,
-      user: null,
-    };
-    this.googleLogin = this.googleLogin.bind(this);
-  }
-
-  async googleLogin() {
-    try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      const { user } = await firebase.auth().signInWithPopup(provider);
-      this.setState({ isLoggedIn: true, user: user });
-      document.write(`Hello ${user.displayName}`);
-      console.log("user", user);
-    } catch (error) {
-      console.log("error has occurred in googleLogin func:", error);
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.isLoggedIn ? (
-          <div>
-            <h1>Welcome to the Firebase AWP, {this.state.user.displayName}!</h1>
-          </div>
-        ) : (
-          <div>
-            <h1>Welcome to the Firebase AWP</h1>
-            <button onclick={this.googleLogin()}>Login with Google</button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Webpage />, document.getElementById("app"));
+ReactDOM.render(<Home />, document.getElementById("app"));
